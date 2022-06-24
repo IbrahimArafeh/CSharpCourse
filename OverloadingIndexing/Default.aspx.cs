98f21af51cb1f2a48741c5db47cccce6ff6ee11f
Ibrahim Arafeh <68711291+IbrahimArafeh@users.noreply.github.com>
2022-06-24 22:21:01 +0400

    Indexing and Overload Indexing

---
+++ OverloadingIndexing/Default.aspx.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace OverloadingIndexing
+{
+    public partial class _Default : Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            Company company = new Company();
+            pText.InnerText = "The Count of Male Employee before set function: " + company["Male"] + "\r\n" +
+                "The Count of Male Employee after set function : " + company["Male"];
+        }
+    }
+}
