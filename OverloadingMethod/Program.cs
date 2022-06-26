d1b58d5e8fde299a6bdb5aa7e5f1f1641743d014
Ibrahim Arafeh <68711291+IbrahimArafeh@users.noreply.github.com>
2022-06-26 13:05:23 +0400

    OverloadingDone

---
+++ OverloadingMethod/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace OverloadingMethod
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            sumNumber(12, 16,new int[] {30,59}); 
+        }
+
+        private static void sumNumber(int Number_1, int Number_2, int[] numbers)
+        {
+            int result = Number_1 + Number_2;
+            if(numbers.Length>0)
+            {
+                foreach (int number in numbers)
+                {
+                    result += number;
+                }
+            }
+            Console.WriteLine(result);
+        }
+
+        private static void sumNumber(int Number_1, int Number_2)
+        {
+            sumNumber(Number_1, Number_2, null);
+        }
+    }
+}
