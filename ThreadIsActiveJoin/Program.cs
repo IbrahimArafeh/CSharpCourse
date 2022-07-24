653013ee48c5ef71174f7e728ac2c7824379f741
Ibrahim Arafeh <68711291+IbrahimArafeh@users.noreply.github.com>
2022-07-24 22:29:52 +0400

    IsAlive and Join
    explain the following :+1:
    1- Threading isAlive
    2- Threading Join

---
+++ ThreadIsActiveJoin/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Threading;
+
+namespace ThreadIsActiveJoin
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Main started");
+
+            Thread T1 = new Thread(ThreadFun1);
+            T1.Start();
+            if(T1.Join(1000))// don't go to second thread until finish from thread 1 
+            {
+                Console.WriteLine("The Thread Funtion 1 is completed 1 second");
+            }
+            else
+            {
+                Console.WriteLine("The Thread Funtion 1 is nooooooooot completed in 1 second");
+            }
+
+            if(T1.IsAlive)
+            {
+                Console.WriteLine("The Thread Funtion 1 is still working");
+            }
+            else
+            {
+                Console.WriteLine("The Thread Funtion 1 is finished working");
+            }
+
+            Thread T2 = new Thread(ThreadFun2);
+            T2.Start();
+            T2.Join();
+            
+
+            Console.WriteLine("Main Completed");
+        }
+
+        public static void ThreadFun1()
+        {
+            Console.WriteLine("Thread Function 1 is started");
+            Thread.Sleep(5000);
+            Console.WriteLine("Thread Function 1 is about to return");
+        }
+        public static void ThreadFun2()
+        {
+            Console.WriteLine("Thread Function 2 is started");
+        }
+        public static void ThreadFun3()
+        {
+            Console.WriteLine("Thread Function 3 is started");
+        }
+    }
+}
