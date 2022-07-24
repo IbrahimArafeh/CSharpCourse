6b2c400726828e40de76a3e530084961dae22022
Ibrahim Arafeh <68711291+IbrahimArafeh@users.noreply.github.com>
2022-07-24 16:27:05 +0400

    Retrieving Data
    1- Retrieving Data  Using  Call Back
    2- Retrieving Data in Type safe manner

---
+++ RetrievingDataUsingCallBackMethod/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Threading;
+using static RetrievingDataUsingCallBackMethod.Program;
+
+namespace RetrievingDataUsingCallBackMethod
+{
+    internal class Program
+    {
+        public delegate void sumOfNumberCallBack(int sumOfNumbers);
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the mumber to sum");
+            int target = Convert.ToInt32(Console.ReadLine());
+
+            sumOfNumberCallBack callBackMethod = new sumOfNumberCallBack(printSumOfNumbers);
+            customer customer = new customer(target,callBackMethod);
+            Thread T = new Thread(new ThreadStart(customer.computeSumOfNumbers));
+            T.Start();
+        }
+
+        public static void printSumOfNumbers(int sum)
+        {
+            Console.WriteLine("Sum of Number is: " + sum);
+        }
+    }
+
+    internal class customer
+    {
+        private int _target;
+        sumOfNumberCallBack _callBackMethod;
+
+        public customer(int target,sumOfNumberCallBack sumOfNumberCallBack)
+        {
+            _target = target;
+            _callBackMethod = sumOfNumberCallBack; 
+        }
+
+        public void computeSumOfNumbers()
+        {
+            int sum = 0;
+            for(int i = 1; i <= _target; i++)
+            {
+                sum = sum + i;
+            }
+
+            if (_callBackMethod != null)
+                _callBackMethod(sum);
+        }
+
+    }
+}
---
+++ ThreadUsingSafeManner/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Threading;
+
+namespace ThreadUsingSafeManner
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the to count to it");
+            int target = Convert.ToInt32(Console.ReadLine());
+            customer cut = new customer(target);
+
+            //Thread T = new Thread(new ThreadStart(cut.printNumbers));
+            Thread T = new Thread(cut.printNumbers); // other type
+            T.Start();
+        }
+    }
+
+    internal class customer
+    {
+        private int _Target;
+        public customer(int target)
+        {
+            _Target = target;
+        }
+
+        public void printNumbers()
+        {
+            for(int i= 0; i < _Target; i++)
+            {
+                Console.WriteLine(i);   
+            }
+        }
+    }
+}
